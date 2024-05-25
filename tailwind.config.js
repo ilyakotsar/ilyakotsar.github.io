72009606828d52b77d84ef61830d33bcbb702e36
Ilya Kotsar <ilyakotsar@yahoo.com>
2024-05-25 13:59:14 +0700

    Update

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      './**/*.html',
+    ],
+    theme: {
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+        serif: ['serif'],
+        mono: ['monospace'],
+      },
+      extend: {},
+    },
+    plugins: [],
+  }
